80f42660cc03c19c378bba86fa8e26fafc03ad35
Michael <naya.ales@gmail.com>
2021-12-01 20:14:25 +0300

    Create project

---
+++ BinarySearchCsharp/BinarySearchCsharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BinarySearchCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
